adae12420003b39e460bff9a8ba134d2a4279d66
Charter <Charter@Charter-PC>
2015-03-27 15:39:52 +0530

    Successfully completed step8:done

--- angularjs/app/js/apps.js
+++ angularjs/app/js/apps.js
@@ -18,9 +18,11 @@ phonecatControllers.controller('PhoneListCtrl', function($scope, $http){
     $scope.orderProp = 'age';
 });
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-   function($scope, $routeParams) {
-     $scope.phoneId = $routeParams.phoneId;
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+   function($scope, $routeParams, $http){
+		$http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+	      $scope.phone = data;
+	    });
 }]);
 phonecatApp.config(['$routeProvider',
 function($routeProvider) {
