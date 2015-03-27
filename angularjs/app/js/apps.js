17c66f25dae092d18eac328cceccc3b543ebd0b0
Charter <Charter@Charter-PC>
2015-03-27 16:22:02 +0530

    Successfully completed step10:done

--- angularjs/app/js/apps.js
+++ angularjs/app/js/apps.js
@@ -7,12 +7,6 @@ var phonecatControllers = angular.module('phonecatControllers', []);
 
 
 phonecatControllers.controller('PhoneListCtrl', function($scope, $http){
-	var phones = [
-	              	{'name': 'Nexus S','snippet': 'Fast just got faster with Nexus S.','age': 1},
-				    {'name': 'Motorola XOOM™ with Wi-Fi','snippet': 'The Next, Next Generation tablet.','age': 2},
-				    {'name': 'MOTOROLA XOOM™','snippet': 'The Next, Next Generation tablet.','age': 3}
-				 ]; 
-	$scope.phones = phones;
 	 $http.get('phones/phones.json').success(function(data) {
 	    $scope.phones = data;
 	  });
@@ -23,7 +17,11 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$h
    function($scope, $routeParams, $http){
 		$http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
 	      $scope.phone = data;
+	      $scope.mainImageUrl = data.images[0];
 	    });
+		$scope.setImage = function(imageUrl) {
+	      $scope.mainImageUrl = imageUrl;
+	    };
 }]);
 phonecatApp.config(['$routeProvider',
 function($routeProvider) {
