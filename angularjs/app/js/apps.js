15e97a5a42873fdb75ad98b437fe51301009745d
Charter <Charter@Charter-PC>
2015-03-27 15:01:18 +0530

    Completed step 6

--- angularjs/app/js/apps.js
+++ angularjs/app/js/apps.js
@@ -1,10 +1,14 @@
 var phonecatApp = angular.module("phonecatApp", []);
 
-phonecatApp.controller('phoneController', function($scope){
+phonecatApp.controller('phoneController', function($scope, $http){
 	var phones = [
-	              	{"name":"Nexus S","description":"Fast just got faster with Nexus S."},
-	              	{"name":"Motorola XOOM™ with Wi-Fi","description":"The Next, Next Generation tablet."},
-	              	{"name":"MOTOROLA XOOM™","description":"The Next, Next Generation tablet."}
-				  ]; 
+	              	{'name': 'Nexus S','snippet': 'Fast just got faster with Nexus S.','age': 1},
+				    {'name': 'Motorola XOOM™ with Wi-Fi','snippet': 'The Next, Next Generation tablet.','age': 2},
+				    {'name': 'MOTOROLA XOOM™','snippet': 'The Next, Next Generation tablet.','age': 3}
+				 ]; 
 	$scope.phones = phones;
+	 $http.get('phones/phones.json').success(function(data) {
+	    $scope.phones = data;
+	  });
+    $scope.orderProp = 'age';
 });
