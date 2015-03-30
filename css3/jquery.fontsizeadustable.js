a1a6eb43007a896fca36487268dcffa84e45ad67
Charter <Charter@Charter-PC>
2015-03-30 17:30:36 +0530

    Created my first jquery plugin:done

---
+++ css3/jquery.fontsizeadustable.js
@@ -0,0 +1,76 @@
+(function ($) {
+    $.fn.fontSizeAdustable = function( options ) {
+        var settings = $.extend({
+        	useCookie: true,
+        	defaultFontSize: 14,
+        	cookieName: "DEFAULT_FONT_SIZE",
+        	restoreButton: true,
+        	restoreConfirm: true,
+        	restoreConfirmMessage: "Are you sure to restore the font settings (Yes/No)",
+        	alreadyRestoreMessage: "Already on default setting!",
+        	incraseFontSizeStep:1,
+        	buttonsIcons:true,
+        	buttonText:true
+        }, options );
+        
+ 		var controls = function (settings){
+ 			return '<div class="fontSizeAdustableControl pull-right">\
+	 			<button type="button" id="increase">+</button>\
+	 			<button type="button" id="decrease">-</button>\
+	 			<button type="button" id="restore">Restore</button>\
+	 		</div>';	
+ 		};
+ 		
+ 		$(document).on("click", ".fontSizeAdustableControl button", function(){
+ 			var t = $(this);
+ 			var font_size = parseInt($("body").css("font-size"));
+ 			if (t.attr("id") == "increase") {
+ 				font_size++;
+ 			}
+ 			else if (t.attr("id") == "restore") {
+ 				if(settings.restoreConfirm) {
+	 				if(font_size == settings.defaultFontSize) {
+	 					alert(settings.alreadyRestoreMessage);
+	 					return;
+	 				}
+ 				}
+ 				var confirm = settings.restoreConfirm ? window.confirm(settings.restoreConfirmMessage) : true;
+ 				if(confirm) {
+ 					font_size = settings.defaultFontSize;
+ 				}
+ 			}
+ 			else {
+ 				font_size--
+ 			}
+ 			setFontSize(font_size);
+ 		});
+ 		
+ 		// set the font size and apply it for the body 
+ 		var setFontSize = function (font_size){
+ 			$("body").css("font-size", font_size+"px");
+ 			settings.useCookie?cookieSetFontSize(font_size):"";
+ 		}
+ 		
+ 		// store the current font size in cookie
+ 		var cookieSetFontSize = function(font_size){
+ 			if(font_size == settings.cookieName) {
+ 				$.removeCookie(settings.cookieName);
+ 			}
+ 			$.cookie(settings.cookieName, font_size, { path: '/', expires: 7 });
+ 		}
+ 		
+ 		// append the controlls for increase, decrease and restore
+        $(this).prepend(controls);
+        
+        if(settings.useCookie) {
+        	
+        	// self invoking function to apply the cookie font size value
+	        (function (settings) {
+	        	if($.cookie(settings.cookieName) !== "undefined"){
+		    		setFontSize($.cookie(settings.cookieName));
+		    	}
+	        })(settings);
+        }
+        return this;
+    };
+}(jQuery));
