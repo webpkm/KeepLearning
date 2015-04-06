bb6162dcb4379611a09e22e0d803c86d089e9e4a
Charter <Charter@Charter-PC>
2015-04-06 17:16:04 +0530

    rootScope always available in scope:done

--- angular/app.js
+++ angular/app.js
@@ -2,6 +2,9 @@ var app = angular.module('myApp', [], function(){
 	
 });
 
+app.run(function($rootScope) {
+  $rootScope.user = {first_name: 'Prem Kumar', last_name: 'Maurya', user_guid:'0001O'}
+});
 app.controller('Page1Controller', function($scope){
 	$scope.page = "Page1Controller";
 });
