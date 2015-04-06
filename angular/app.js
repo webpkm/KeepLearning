185c0465e8d8eb1428693fd45ed6c58005d2da4f
Charter <Charter@Charter-PC>
2015-04-06 17:13:35 +0530

    added the rootscope controller

---
+++ angular/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('myApp', [], function(){
+	
+});
+
+app.controller('Page1Controller', function($scope){
+	$scope.page = "Page1Controller";
+});
+
+app.controller('Page2Controller', function($scope){
+	$scope.page = "Page2Controller";
+});
